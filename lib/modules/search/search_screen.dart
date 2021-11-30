a27178c05d6dcb4dcbf87c4c95102b76c3d463f9
Aasem <assem_soma541@yahoo.com>
2021-11-30 09:07:04 +0200

    Search Screen Added

--- lib/modules/search/search_screen.dart
+++ lib/modules/search/search_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/shared/cupit/app_cubit.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({Key? key}) : super(key: key);
@@ -8,7 +10,22 @@ class SearchScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: TextField(
-          decoration: InputDecoration(border: OutlineInputBorder()),
+          decoration: InputDecoration(
+            hintText: 'Search',
+            hintStyle: TextStyle(
+                color: BlocProvider.of<AppCubit>(context).isDark ? Colors.white : Colors.black),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: const BorderSide(
+                color: Colors.deepOrange,
+                width: 2,
+              ),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: const BorderSide(color: Colors.deepOrange),
+            ),
+          ),
         ),
       ),
       body: ListView.builder(
